ad0d1f2fe85ab4f444999eff1c3995d72c300033
i.vukolov <i.vukolov@Vrgaz.ru>
2025-04-15 17:05:40 +0300

    added posts field

--- yatube_api/posts/serializers.py
+++ yatube_api/posts/serializers.py
@@ -9,14 +9,18 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = ['author', 'post', 'text']
 
 
+class GroupSerializer(serializers.ModelSerializer):
+    posts = serializers.StringRelatedField()
+    class Meta:
+        model = Group
+        fields = ['title', 'slug', 'description', 'posts']
+
+
 class PostSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True)
+    group = GroupSerializer(many=True)
 
     class Meta:
         model = Post
         fields = ['text', 'author', 'image', 'group', 'comments']
 
-class GroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Group
-        fields = '__all__'
