88b80e7c2d70ef9303decb00a0d6b2c16580eb5d
i.vukolov <i.vukolov@Vrgaz.ru>
2025-04-17 09:51:03 +0300

    remove rest_framework.exceptions.MethodNotAllowed

--- yatube_api/posts/views.py
+++ yatube_api/posts/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import action
 from rest_framework import viewsets, status
-from rest_framework.exceptions import PermissionDenied, MethodNotAllowed
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
