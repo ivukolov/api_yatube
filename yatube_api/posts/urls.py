483ac5519595bbb8cccaa0e58b4b04271adaba24
i.vukolov <i.vukolov@Vrgaz.ru>
2025-04-17 09:35:05 +0300

    pep fix v3

--- yatube_api/posts/urls.py
+++ yatube_api/posts/urls.py
@@ -1,14 +1,12 @@
 from django.urls import include, path
 from rest_framework.authtoken import views
-from rest_framework.routers import DefaultRouter, SimpleRouter
+from rest_framework.routers import DefaultRouter
 
 from .views import PostViewSet, GroupViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'groups', GroupViewSet)
-
-
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
